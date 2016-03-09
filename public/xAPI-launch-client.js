04395591e54833dd6252a06af043a8bf14038a3f
rchadwic <rchadwic@gmail.com>
2016-03-09 10:47:35 -0500

    add code to augment links with launch data

--- public/xAPI-launch-client.js
+++ public/xAPI-launch-client.js
@@ -30,45 +30,100 @@ function cb_wrap(cb)
     }
 }
 
+
+//The library will append the necessary launch info to each new A that is linked to the page. 
+//NOTE: This cannot work if you programmatically change the window location. If you do, you must
+//execute the logic in setupCourseLinks to send the initialization data to the new location (if
+//you wish that new location to track as part of this session)
+function observeForNewLinks()
+{
+    // select the target node
+    var target = document.body;
+    // create an observer instance
+    var observer = new MutationObserver(function(mutations)
+    {  
+        mutations.forEach(function(mutation)
+        {
+            for(var i in mutation.addedNodes)
+            {
+                if(mutation.addedNodes[i].constructor == HTMLAnchorElement)
+                {
+                    var node = $(mutation.addedNodes[i]);
+                    setupCourseLinks(node);
+                }
+            }
+        });
+    });
+    // configuration of the observer:
+    var config = {
+        attributes: true,
+        childList: true,
+        subtree:true
+    };
+    // pass in the target node, as well as the observer options
+    observer.observe(target, config);
+    // later, you can stop observing
+  ///  observer.disconnect();
+}
+//This library will init all links in the DOM that include the attribute "courseLink = true"
+//with the information necessary for the document at that link to track as part of this session.
+function setupCourseLinks($nodes)
+{
+    var launchToken = getQueryVariable("xAPILaunchKey");
+    var launchEndpoint = getQueryVariable("xAPILaunchService");
+    var encrypted = getQueryVariable("encrypted");
+    var query = "xAPILaunchKey=" + launchToken + "&xAPILaunchService=" + launchEndpoint;
+    if (encrypted)
+    {
+        query += "&encrypted=true";
+    }
+    $nodes.each(function(e)
+    {
+        var link = $(this);
+        var href = link.attr('href');
+        var courseLink = link.attr('courseLink');
+        if (courseLink == "true")
+        {
+            if (href.indexOf("?") > -1)
+            {
+                href = href + "&" + query;
+            }
+            else
+                href = href + "?" + query;
+            link.attr('href', href);
+        }
+    });
+}
+
 function xAPILaunch(cb, terminate_on_unload)
 {
     cb = cb_wrap(cb);
     try
     {
         $(document.body).append("<script src='/static/xapiwrapper.min.js'></script>");
-
         var launchToken = getQueryVariable("xAPILaunchKey");
         var launchEndpoint = getQueryVariable("xAPILaunchService");
-
         var encrypted = getQueryVariable("encrypted");
         if (!encrypted)
         {
             //here, we'd have to implement decryption for the data. This makes little sense in a client side only course
         }
-
         if (!launchToken || !launchEndpoint)
             return cb("invalid launch parameters");
         var launch = new URL(launchEndpoint);
         launch.pathname += "launch/" + launchToken;
-        $.post(launch.toString(), function() {
-
-        }).success(function(body, xhr)
+        $.post(launch.toString(), function() {}).success(function(body, xhr)
         {
             var launchData = body;
-
             var conf = {};
             conf['endpoint'] = launchData.endpoint;
             conf["actor"] = launchData.actor;
-
             window.onunload = function()
             {
-
                 if (!terminate_on_unload)
                     return;
-
                 var launch = new URL(launchEndpoint);
                 launch.pathname += "launch/" + launchToken + "/terminate";
-
                 $.ajax(
                 {
                     url: launch.toString(),
@@ -76,11 +131,14 @@ function xAPILaunch(cb, terminate_on_unload)
                     method: "POST",
                     contentType: "application/json",
                     data: '{"code":0,"description":"User closed content"}'
-
                 })
-
             }
             ADL.XAPIWrapper.changeConfig(conf);
+
+            //Links that include "courseLink='true'"
+            setupCourseLinks($(document.body).find("a"));
+            //Also, if links are added dynamically, we will do the same logic for those links.
+            observeForNewLinks();
             return cb(null, body, ADL.XAPIWrapper);
         }).error(function(err)
         {

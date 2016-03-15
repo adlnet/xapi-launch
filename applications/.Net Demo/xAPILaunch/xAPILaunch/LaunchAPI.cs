d8f899f4835f4b15db25df351e1ae1924f5450af
rchadwic <rchadwic@gmail.com>
2016-03-15 17:15:44 -0400

    begin standalone app demo

---
+++ applications/.Net Demo/xAPILaunch/xAPILaunch/LaunchAPI.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+
+namespace xAPILaunch
+{
+    class xAPIActor
+    {
+        public string mBox;
+        public string name;
+    }
+    class LaunchData
+    {
+        public xAPIActor Actor;
+        public int launchCode;
+    }
+    class LaunchAPI
+    {
+        public LaunchData Launch(string launchUrl, string launchToken)
+        {
+              var _launchData = new LaunchData();
+
+                
+
+
+
+              return _launchData;
+        }
+    }
+}
---
+++ applications/.Net Demo/xAPILaunch/xAPILaunch/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            StartBtn.IsEnabled = false;
+            Question1Tab.IsEnabled = false;
+            Question2Tab.IsEnabled = false;
+            Question3Tab.IsEnabled = false;
+            FinishTab.IsEnabled = false;
+        }
+
+        private void LaunchParam_Changed(object sender, TextChangedEventArgs e)
+        {
+            if (LaunchServerTxt.Text.Length > 0 && LaunchTokenTxt.Text.Length > 0)
+            {
+                Uri result;
+                if (Uri.TryCreate(LaunchServerTxt.Text, UriKind.Absolute, out result))
+                {
+                    StartBtn.IsEnabled = true;
+                    lblFeedback.Content = "";
+                }
+                else
+                {
+                    StartBtn.IsEnabled = false;
+                    lblFeedback.Content = "Please use the absolute URL for your Launch Server";
+                }
+            }
+            else
+            {
+                lblFeedback.Content = "Please enter both the Launch Token and the Launch Server URL";
+            }
+        }
+    }
+}
